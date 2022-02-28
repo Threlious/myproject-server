6d0be81bf8dfe195f0063b78b9ae4ea1dfbb5f87
Garret <ma_melnik97@mail.ru>
2022-02-28 13:24:15 +0300

    image add for profile

--- users/views.py
+++ users/views.py
@@ -43,7 +43,13 @@ def registration(request):
 
 
 def profile(request):
-    form = UserProfileForm(instance=request.user)
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
     context = {
         'title': 'GeekShop - Профиль',
         'form': form
