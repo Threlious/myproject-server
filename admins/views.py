3368223d39c3c854ba8fd6f436e468af567293ba
Garret <ma_melnik97@mail.ru>
2022-03-16 09:22:59 +0300

    init admins app

---
+++ admins/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+def index(request):
+    context = {
+        'title': 'GeekShop - Админ-панель'
+    }
+    return render(request, 'admins/index.html', context)
