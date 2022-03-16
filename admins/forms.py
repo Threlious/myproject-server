61bf3aa1e58a04e8c2c4031ab3008f6861c33e4c
Garret <ma_melnik97@mail.ru>
2022-03-16 11:08:54 +0300

    added user admin update controller

--- admins/forms.py
+++ admins/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from users.forms import UserRegistrationForm
+from users.forms import UserRegistrationForm, UserProfileForm
 from users.models import User
 
 
@@ -11,3 +11,7 @@ class UserAdminRegistrationForm(UserRegistrationForm):
         model = User
         fields = ('first_name', 'last_name', 'image', 'username', 'email', 'password1', 'password2')
 
+
+class UserAdminProfileForm(UserProfileForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control py-4'}))
--- admins/views.py
+++ admins/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 
 from users.models import User
-from admins.forms import UserAdminRegistrationForm
+from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 
 
 def index(request):
@@ -36,3 +36,21 @@ def admin_users_create(request):
         'form': form
     }
     return render(request, 'admins/admin-users-create.html', context)
+
+
+# Update controller
+def admin_users_update(request, pk):
+    selected_user = User.objects.get(id=pk)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(instance=selected_user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admin_staff:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=selected_user)
+    context = {
+        'title': 'GeekShop - Admin',
+        'form': form,
+        'selected_user': selected_user,
+    }
+    return render(request, 'admins/admin-users-update-delete.html', context)
