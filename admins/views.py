932502d7ecfc0824d5d293e7609da779a7f3ac4d
Garret <ma_melnik97@mail.ru>
2022-03-16 10:42:40 +0300

    admin create users

--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 
 from users.models import User
+from admins.forms import UserAdminRegistrationForm
+
 
 def index(request):
     context = {
@@ -9,6 +12,7 @@ def index(request):
     return render(request, 'admins/index.html', context)
 
 
+# Read controller
 def admin_users(request):
     users = User.objects.all()
     context = {
@@ -16,3 +20,19 @@ def admin_users(request):
         'users': users,
     }
     return render(request, 'admins/admin-users-read.html', context)
+
+
+# Create controller
+def admin_users_create(request):
+    if request.method == 'POST':
+        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admin_staff:admin_users'))
+    else:
+        form = UserAdminRegistrationForm()
+    context = {
+        'title': 'GeekShop - Admin',
+        'form': form
+    }
+    return render(request, 'admins/admin-users-create.html', context)
