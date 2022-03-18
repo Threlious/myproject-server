48f1438f85e633b526454f4431049e316d320cf9
Garret <ma_melnik97@mail.ru>
2022-03-18 17:12:48 +0300

    decorator for view class

--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
+from django.utils.decorators import method_decorator
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
@@ -19,6 +20,15 @@ def index(request):
 class UserAdminListView(ListView):
     model = User
     template_name = 'admins/admin-users-read.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(UserAdminListView, self).get_context_data(object_list=None, **kwargs)
+        context['title'] = 'GeekShop - Admin'
+        return context
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserAdminListView, self).dispatch(request, *args, **kwargs)
 
 
 class UserAdminCreateView(CreateView):
@@ -44,13 +54,6 @@ class UserAdminDeleteView(DeleteView):
         self.object = self.get_object()
         self.object.safe_delete()
         return HttpResponseRedirect(self.success_url)
-
-# # Delete controller
-# @user_passes_test(lambda u: u.is_staff)
-# def admin_users_delete(request, pk):
-#     user = User.objects.get(id=pk)
-#     user.safe_delete()
-#     return HttpResponseRedirect(reverse('admin_staff:admin_users'))
 
 # # Read controller
 # @user_passes_test(lambda u: u.is_staff)
@@ -95,3 +98,10 @@ class UserAdminDeleteView(DeleteView):
 #         'selected_user': selected_user,
 #     }
 #     return render(request, 'admins/admin-users-update-delete.html', context)
+
+# # Delete controller
+# @user_passes_test(lambda u: u.is_staff)
+# def admin_users_delete(request, pk):
+#     user = User.objects.get(id=pk)
+#     user.safe_delete()
+#     return HttpResponseRedirect(reverse('admin_staff:admin_users'))
